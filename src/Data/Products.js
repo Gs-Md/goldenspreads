5b01303f077228e5a563541f8981430548c59cb2
Gs-Md <42230938@students.liu.edu.lb>
2026-02-17 21:19:59 +0200

    .

--- src/Data/Products.js
+++ src/Data/Products.js
@@ -1,8 +1,9 @@
-import creamy from "../Assets/creamy.png";
-import crunchy from "../Assets/crunchy.png";
-import cocoa from "../Assets/cocoa.png";
-import dates from "../Assets/dates.png";
-import protein from "../Assets/protein.png";
+import creamy from "../Assets/creamy.jpg";
+import crunchy from "../Assets/crunchy.jpg";
+import cocoa from "../Assets/cocoa.jpg";
+import dates from "../Assets/dates.jpg";
+import protein from "../Assets/protein.jpg";
+import comingSoon from "../Assets/comingsoon.jpg";
 
 const products = [
   {
@@ -20,7 +21,7 @@ const products = [
     name: "Crunchy Peanut Butter",
     price: "$4.00",
     weight: "300 g",
-    image: crunchy,
+    image: comingSoon,
     ingredients: "Roasted peanuts, Peanut pieces",
     info: ["No Added Sugars", "Gluten Free", "Keto Friendly","Organic"],
     bestUse: "Toast, Yogurt bowls, Apple slices, Banana sandwiches, OatMeals.",
@@ -30,7 +31,7 @@ const products = [
     name: "Cocoa Peanut Butter",
     price: "$6.00",
     weight: "300 g",
-    image: cocoa,
+    image: comingSoon,
     ingredients: "Peanuts, Cocoa powder",
     info: ["Palm Oil Free", "Gluten Free", "No Added Sugars", "No Preservatives"],
     bestUse: "Toast, Pancake, Apple slices, Banana sandwiches, Smoothies, OatMeals.",
@@ -42,7 +43,7 @@ const products = [
     name: "Dates Peanut Butter",
     price: "$6.00",
     weight: "300 g",
-    image: dates,
+    image: comingSoon,
     ingredients: "Peanuts, Dates",
     info: ["Organic", "Naturally Sweetened", "Gluten Free","Natural Energy Booster"],
     bestUse: "Toast, Pancake,Yogurt Bowls, Energy Balls, Smoothies, OatMeals.",
@@ -52,7 +53,7 @@ const products = [
     name: "Protein Powder Peanut Butter",
     price: "$8.00",
     weight: "300 g",
-    image: protein,
+    image: comingSoon,
     ingredients: "Peanuts, whey protein",
     info: ["High Protein (130g)", "Palm Oil Free", "No Preservatives","Natural Energy Booster"],
     bestUse: "Smoothies, OatMeals, Protein shakes, Waffles, Pancakes, Rice Cake.",
